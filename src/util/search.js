fc1e71592e7bf181cdb9bdad69523a239217ba96
jdotrjs <jdotrjs@users.noreply.github.com>
2018-09-21 01:00:12 -0700

    include comments

--- src/util/search.js
+++ src/util/search.js
@@ -2,6 +2,21 @@ import fuzzy from 'fuzzy'
 
 import { issues } from './stub'
 
+/**
+ * search returns items out of the Tutorials and Update lists of a newsletter
+ * that match the search string.  This checks the search string in a fuzzy
+ * manner against the Tutorial name and description and the update string.
+ * The result returns the complete Tutorial object and update string as
+ * described below.
+ *
+ * @param {string} searchString - the thing you want to find
+ * @param {object} data - the newsletter yaml; no concrete format right now
+ * @param {string} tutVersion - this is matched vs the `version` attribute of
+ *   a tutorial to limit results to a specific version
+ * @returns { [issueNumber: number]: { tutorials: object, updates: string} }
+ *   A mapping of the issue number to an object containing the tutorials and
+ *   update summaries that matched.
+ */
 const search = (searchString, data, tutVersion) =>
   data.reduce(
     (acc, cur) => {
