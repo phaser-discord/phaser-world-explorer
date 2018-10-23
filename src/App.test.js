f8cbcfb0d9e83e93b6e55da2dfd452e9fdb3b3b4
Patrick Sletvold <patricksletvold@hotmail.com>
2018-10-23 19:12:01 +0200

    Add mock functions for the matchMedia API

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Add mock functions for the matchMedia API
+window.matchMedia = () => ({
+  addListener: () => { },
+  removeListener: () => { }
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
