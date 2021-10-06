2208a3c2daee75768701f95464b1c876eed39526
Patrick Sletvold <patricksletvold@hotmail.com>
2021-10-06 20:22:10 +0200

    Fix NewsletterListItem

--- src/components/NewsletterListItem.js
+++ src/components/NewsletterListItem.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NewsletterListItem = ({ item: { issueNumber } = {} }) => {
-  return (
-    issueNumber && (
-      <Link to={`/newsletter/issue/${issueNumber}`}>Issue {issueNumber}</Link>
-    )
-  );
+  return issueNumber ? (
+    <Link to={`/newsletter/issue/${issueNumber}`}>Issue {issueNumber}</Link>
+  ) : null;
 };
 
 export default NewsletterListItem;
