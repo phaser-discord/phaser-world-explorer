def8259d93dad31856d1ca2067e2085c9b44fef6
Patrick Sletvold <patricksletvold@hotmail.com>
2021-10-06 20:13:39 +0200

    Update supported browsers and remove polyfill

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './App';
 
-import './polyfills/ArrayFind';
-
 // Only need to wory about basename when not using HashRouter
 // const basename = process.env.REACT_APP_BASENAME
 const basename = null;
